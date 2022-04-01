700b81dfa111d7519f34e325148aacb065fe1b93
jacobyoung20 <39632608+jacobtnyoung@users.noreply.github.com>
2022-04-01 09:42:23 -0700

    updates

--- projects/R2PhX_AUTOMATOR_script.R
+++ projects/R2PhX_AUTOMATOR_script.R
@@ -10,6 +10,9 @@ render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHXcrime_type
 render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHX_heat_shelter_2020.Rmd", "html_document" )
 render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHXcrime_police_shootings.Rmd", "html_document" )
 
+# This runs the index file.
+render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/index.Rmd", "html_document" )
+
 # working through.
 #render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHXcrime_GEO.Rmd", "html_document" )
 
