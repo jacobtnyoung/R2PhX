ea251ed3a8874f38487d0e5921e95517b1ae8b9f
jacobyoung20 <39632608+jacobtnyoung@users.noreply.github.com>
2022-11-04 11:56:56 -0700

    October updates

--- R2PhX_AUTOMATOR_script.R
+++ R2PhX_AUTOMATOR_script.R
@@ -1,17 +1,27 @@
-require( knitr )
-require( rmarkdown )
+# ############################ #
+# Script to run R2PhX pages ----
+# ############################ #
+
+
+# Load the libraries.
+library( knitr )
+library( rmarkdown )
+library( here )
+
 
 # Creates the html output.
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHXcrime.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHXcrime_trends.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHXcrime_types.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHXcrime_types_2020.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHX_heat_shelter_2020.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHXcrime_police_shootings.Rmd", "html_document" )
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/projects/PHXcrime_homicide.Rmd", "html_document" )
+render( here::here( "projects/PHXcrime.Rmd" ), "html_document" )
+render( here::here( "projects/PHXcrime_trends.Rmd" ), "html_document" )
+render( here::here( "projects/PHXcrime_types.Rmd" ), "html_document" )
+render( here::here( "projects/PHXcrime_types_2020.Rmd" ), "html_document" )
+render( here::here( "projects/PHX_heat_shelter_2020.Rmd" ), "html_document" )
+render( here::here( "projects/PHXcrime_police_shootings.Rmd" ), "html_document" )
+render( here::here( "projects/PHXcrime_homicide.Rmd" ), "html_document" )
+
 
 # This runs the index file.
-render( "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/R2PhX/index.Rmd", "html_document" )
+render( here::here( "index.Rmd" ), "html_document" )
+
 
 # Close this script.
 quit( save = "no" )
